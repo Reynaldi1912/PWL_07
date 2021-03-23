fe2fa7079ad0363498c84b14a21b6671cf18d0d7
= <nexrey19@gmail.com>
2021-03-23 08:33:10 +0700

    create table mahasiswa

---
+++ database/migrations/2021_03_23_012827_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table-> string('nim')->primaryKey();
+            $table -> string('nama');
+            $table -> string('kelas');
+            $table -> string('jurusan');
+            $table -> string('no_handphone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
