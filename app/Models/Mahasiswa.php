eefac5a899baed1f7ea30c4861a98a604d7eaea6
= <nexrey19@gmail.com>
2021-03-23 10:39:39 +0700

    add fillable

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
 
 class Mahasiswa extends Model
 {
     use HasFactory;
+    protected $table = 'Mahasiswa';
+    public $timestamps= false; 
+    protected $primaryKey = 'nim';
+
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+        'No_Handphone',
+        ];
 }
