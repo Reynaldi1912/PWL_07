5a94637ab36d317d1cb6b12f0768e203a5fbb2df
= <nexrey19@gmail.com>
2021-03-25 15:21:54 +0700

    add input searching

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,9 +5,35 @@
  <div class="pull-left mt-2">
  <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
  </div>
- <div class="float-right my-2">
+ <div>
+ <div class="float-left my-2">
  <a class="btn btn-success" href="{{ route('mahasiswa.create') }}"> Input Mahasiswa</a>
  </div>
+ <div>
+        <div class="mx-auto pull-right">
+            <div class="">
+                <form action="{{ route('mahasiswa.index') }}" method="GET" role="search">
+                    <div class="input-group">
+                        <span class="input-group-btn mr-5 mt-1">
+                            <button class="btn btn-info" type="submit" title="Search projects">
+                                <span class="fas fa-search"></span>
+                            </button>
+                        </span>
+                        <input type="text" class="form-control mr-2" name="term" placeholder="Search projects" id="term">
+                        <a href="{{ route('mahasiswa.index') }}" class=" mt-1">
+                            <span class="input-group-btn">
+                                <button class="btn btn-danger" type="button" title="Refresh page">
+                                    <span class="fas fa-sync-alt"></span>
+                                </button>
+                            </span>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+ </div>
+
  </div>
  </div>
  
@@ -16,11 +42,7 @@
  <p>{{ $message }}</p>
  </div>
  @endif
- <p>Cari Data Mahasiswa :</p>
-	<form action="/mahasiswa/cari" method="GET">
-		<input type="text" name="cari" placeholder="Cari Mahasiswa">
-		<input type="submit" value="cari">
-	</form>
+ 
  <table class="table table-bordered">
  <tr>
  <th>Nim</th>
